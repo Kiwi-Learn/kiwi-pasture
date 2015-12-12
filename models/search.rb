1b1097b3fcf96e15adb1e17d19ca56256616817b
Che-Wei Lin <linton.tw@gmail.com>
2015-12-13 02:55:52 +0800

    fixed typo for data type

--- models/search.rb
+++ models/search.rb
@@ -3,11 +3,11 @@ require 'dynamoid'
 class Search
   include Dynamoid::Document
   field :keyword, :string
-  field :course_name, :datetime
+  field :course_name, :string
   field :course_url, :string
   field :course_id, :string
   field :course_date, :string
-  
+
   def self.destroy(id)
     find(id).destroy
   end
