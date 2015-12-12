71a2f741303d80dc1c87b2ed11a0ef301515f773
Che-Wei Lin <linton.tw@gmail.com>
2015-12-12 23:35:56 +0800

    fixed test

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,10 +1,29 @@
 require_relative 'spec_helper'
+require_relative 'support/story_helpers'
 require 'json'
 
-describe 'Getting the root of the service' do
-  it 'Should return ok' do
-    get '/'
-    last_response.must_be :ok?
-    last_response.body.must_match(/Kiwi farm service/)
+describe 'KiwiPasture Stories' do
+  include StoryHelpers
+  describe 'Getting the root of the service' do
+    it 'Should return ok' do
+      get '/api/v2'
+      last_response.must_be :ok?
+      last_response.body.must_match(/Kiwi pasture service/)
+    end
+  end
+
+  describe 'Getting the detail of course' do
+    it 'Should return ok' do
+      get '/api/v2/info/MA02004.json'
+      last_response.must_be :ok?
+      last_response.body.must_match(/"id":"MA02004"/)
+    end
+  end
+
+  describe 'Course List' do
+    it 'Should return course list' do
+      get '/api/v2/courselist'
+      last_response.must_be :ok?
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,24 +1,13 @@
 ENV['RACK_ENV'] = 'test'
 
-Dir.glob('./{models,helpers,controllers}/*.rb').each { |file| require file }
-
 require 'minitest/autorun'
 require 'rack/test'
-require 'vcr'
-require 'webmock/minitest'
+Dir.glob('./{config,models,services,controllers}/init.rb').each do |file|
+  require file
+end
 
 include Rack::Test::Methods
 
 def app
-  ApplicationController
-end
-
-def random_str(n)
-  srand(n)
-  (0..n).map { ('a'..'z').to_a[rand(26)] }.join
-end
-
-VCR.configure do |config|
-  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
-  config.hook_into :webmock
+  KiwiPasture
 end
