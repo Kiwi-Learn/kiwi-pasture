521599bed4d0a21bdfa956b1f4cbd64fa1a17c1d
Che-Wei Lin <linton.tw@gmail.com>
2015-12-13 02:55:25 +0800

    wrapper up for creating model

--- controllers/search_helpers.rb
+++ controllers/search_helpers.rb
@@ -1,5 +1,15 @@
 module SearchHelpers
-  def search_course(keyword)
+  def new_search(req, scraped_result)
+    search = Search.new
+    search.keyword = req['keyword']
+    search.course_name = scraped_result.name
+    search.course_id = scraped_result.id
+    search.course_url = scraped_result.url
+    search.course_date = scraped_result.date
+    search
+  end
+
+  def scraper_search_course(keyword)
     Course.new(nil, keyword)
   rescue
     halt 404
