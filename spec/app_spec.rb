478980bbe456e62e0fa3d12d22821951dd13607b
Che-Wei Lin <linton.tw@gmail.com>
2015-12-13 02:56:03 +0800

    Add test

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -27,3 +27,34 @@ describe 'KiwiPasture Stories' do
     end
   end
 end
+
+
+describe 'Searching course' do
+  before do
+    Search.delete_all
+  end
+
+  it 'Should return course info' do
+    header = { 'CONTENT_TYPE' => 'application/json' }
+    body = {
+      keyword: 'program'
+    }
+
+    # Check redirect URL from post request
+    post '/api/v2/search', body.to_json, header
+    last_response.must_be :redirect?
+    next_location = last_response.location
+    # next_location.must_match %r{api\/v2\/searched\/\d+}
+
+    # Check if request parameters are stored in ActiveRecord data store
+    ser_id = next_location.scan(%r{searched\/(\d+)}).flatten[0].to_i
+    saved_keyword = Search.where(:keyword => body[:keyword]).all[0].keyword
+    saved_keyword.must_equal body[:keyword]
+
+    follow_redirect!
+    # last_request.url.must_match %r{api\/v2\/searched\/\d+}
+
+    # Check if redirected response has results
+    JSON.parse(last_response.body).count.must_be :>, 0
+  end
+end
